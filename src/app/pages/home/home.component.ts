ecd77dd714a82d9271ae8d8ee95b1ad4570de2f5
dasinb0 <94384375+dasinb0@users.noreply.github.com>
2023-09-14 19:59:50 -0300

    Agregue product-box

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+const ROWS_HEIGHT: {[id:number]: number} = {1: 400, 3: 355, 4: 350};
+
 @Component({
   selector: 'app-home',
   templateUrl:'./home.component.html' 
@@ -7,6 +9,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit{
 cols = 3;
+rowHeight = ROWS_HEIGHT[this.cols];
 category: string | undefined;
   constructor() {
 
@@ -18,6 +21,7 @@ category: string | undefined;
 
   onColumnsCountChange(colsNum: number): void{
      this.cols = colsNum;
+     this.rowHeight = ROWS_HEIGHT[this.cols];
   }
 
   onShowCategory(newCategory: string):void {
